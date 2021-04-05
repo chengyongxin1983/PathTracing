54ab2ae9d870cde70e9b080461cf0a87698a6919
rogercheng <rogercheng@tencent.com>
2021-04-05 20:07:01 +0800

    pt first commit

---
+++ Assets/Scripts/PTMaterial.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+[StructLayout(LayoutKind.Explicit)]
+public unsafe struct PTMaterial
+{
+    [FieldOffset(0)]
+    public Vector3 Albedo;
+    [FieldOffset(12)]
+    public float metallic;
+    [FieldOffset(16)]
+    public float smoothness;
+    [FieldOffset(20)]
+    public int isLight;
+}
